70ba9012a35e975514bb11a71cfa715655b15efa
Langsrud <oyl@ssb.no>
2021-03-09 14:52:54 +0100

    Fix PxWebApiData-tests after update

--- tests/testthat/test-PxWebApiData.R
+++ tests/testthat/test-PxWebApiData.R
@@ -1,19 +1,23 @@
-test_that("ApiDataLabel and ApiDataId works", {
+test_that("(ApiData1 and ApiData2 works", {
   a <- list(ApiData1(4861, Region = c("1103", "0301"), ContentsCode = "Bosatte", Tid = c(1, -1)),
             ApiData2(4861, Region = c("1103", "0301"), ContentsCode = "Bosatte", Tid = c(1, -1)))
   
-  b <-      ApiData(4861, Region = c("1103", "0301"), ContentsCode = "Bosatte", Tid = c(1, -1))
+  b <-      PxWebApiData::ApiData(4861, Region = c("1103", "0301"), ContentsCode = "Bosatte", Tid = c(1, -1))
   names(b) <- NULL
+  comment(a[[1]]) <- NULL
+  comment(a[[2]]) <- NULL
   expect_identical(a,b)    
 })
 
 
-test_that("ApiDataLabel and ApiDataId works", {
+test_that("GetApiData1 and GetApiData2 works", {
   url1066en <- "https://data.ssb.no/api/v0/dataset/1066.json?lang=en"
   GetApiData1(url1066en)
   GetApiData2(url1066en)
   a <- list(GetApiData1(url1066en), GetApiData2(url1066en))
-  b <- GetApiData(url1066en)
+  b <- PxWebApiData::GetApiData(url1066en)
   names(b) <- NULL
+  comment(a[[1]]) <- NULL
+  comment(a[[2]]) <- NULL
   expect_identical(a,b)    
 })
